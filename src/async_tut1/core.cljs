a66bce3af8400977e70c47b6c1911dfda18869a1
Lawrence Allan Jones <mrwizard82d1@gmail.com>
2015-11-07 19:02:20 -0600

    Listen for event and publish on channel.

--- src/async_tut1/core.cljs
+++ src/async_tut1/core.cljs
@@ -1,9 +1,22 @@
 (ns async-tut1.core
-  (:require [clojure.browser.repl :as repl]))
-
-;; (defonce conn
-;;   (repl/connect "http://localhost:9000/repl"))
+  (:require-macros [cljs.core.async.macros :refer [go]])
+  (:require [goog.dom :as dom]
+            [goog.events :as events]
+            [cljs.core.async :refer [put! chan <!]]))
 
 (enable-console-print!)
 
 (println "Hello world!")
+
+(defn listen [e1 type]
+  (let [out (chan)]
+    ;; Listen to events 
+    (events/listen e1 type
+                   (fn [e]
+                     (put! out e)))
+    out))
+
+;; Test the `listen` function.
+(let [clicks (listen (dom/getElement "search") "click")]
+  (go (while true
+        (println (<! clicks)))))
