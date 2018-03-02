4dc9dec217cc73ecade6d1e46ed9bfc9c72d8260
Larry Jones <larryj@blinds.com>
2018-03-02 16:19:32 -0600

    Updated versions

--- project.clj
+++ project.clj
@@ -1,13 +1,12 @@
 (defproject async-tut1 "0.1.0-SNAPSHOT"
   :description "FIXME: write this!"
   :url "http://example.com/FIXME"
-  :dependencies [[org.clojure/clojure "1.7.0"]
-                 [org.clojure/clojurescript "1.7.170" :classifier "aot"
+  :dependencies [[org.clojure/clojure "1.8.0"]
+                 [org.clojure/clojurescript "1.9.521"
                   :exclusion [org.clojure/data.json]]
-                 [org.clojure/core.async "0.1.256.0-1bf8cf-alpha"]
-                 [org.clojure/data.json "0.2.6" :classifier "aot"]]
-  :jvm-opts ^:replace ["-Xmx1g" "-server"]
-  :plugins [[lein-npm "0.6.1"]]
+                 [org.clojure/core.async "0.3.443"]
+                 [org.clojure/data.json "0.2.6"]]
+  :plugins [[lein-npm "0.6.2"]]
   :npm {:dependencies [[source-map-support "0.3.2"]]}
   :source-paths ["src" "target/classes"]
   :clean-targets ["out" "release"]
